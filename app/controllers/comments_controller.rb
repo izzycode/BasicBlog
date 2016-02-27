35e6ff34c8b8baacb4def707390a6a7f80109326
marcemc <marcemc@yahoo.com>
2016-02-27 14:43:08 -0500

    added basic authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+  thhp_basic_authenticate_with name: "izzy", password: "password", only: :destroy
+  def create
+    @article = Article.find(params[:article_id])
+    @commet = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+  
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment= @article.comments.find(params[:id])
+    redirect_to article_path(@article)
+  end
+  
+  private
+  def comment_params
+    params.require(:comment).permit(:commenter,:body)
+  end
 end
